194799a542dda40ba190322e174c7f28ad90c5c1
Baron-Chen <baronchen1350@gmail.com>
2018-10-17 18:11:25 +0800

    Modified the name of BigFraction and namespace. Add test of BigFraction methods. Constructor for Double has not been modified as I tried the recommanded method but did not work, will check it soon.

--- BigFractionTest/BigFractionTest.cs
+++ BigFractionTest/BigFractionTest.cs
@@ -2,7 +2,7 @@ using System;
 using Xunit;
 using System.Numerics;
 using Xunit.Abstractions;
-using BigFractionExt;
+using Aprismatic;
 
 namespace ConstructorTest
 {
@@ -19,28 +19,24 @@ namespace ConstructorTest
         public void IntConstructor()
         {
             BigFraction a = new BigFraction(-10);
-            output.WriteLine(a.ToString());
         }
 
         [Fact(DisplayName = "Constructor for Double")]
         public void DouConstructor()
         {
             BigFraction a = new BigFraction(6545.99);
-            output.WriteLine(a.ToString());
         }
 
         [Fact(DisplayName = "Constructor for Decimal")]
         public void DecConstructor()
         {
             BigFraction a = new BigFraction(new decimal(8984.65));
-            output.WriteLine(a.ToString());
         }
 
         [Fact(DisplayName = "Constructor for BigInteger")]
         public void BigIntConstructor()
         {
             BigFraction a = new BigFraction(new BigInteger(33), new BigInteger(99));
-            output.WriteLine(a.ToString());
         }
     }
 }
@@ -165,10 +161,11 @@ namespace OpearatorTest
         public void Addition()
         {
             BigFraction a = new BigFraction(1000.25);
-            BigFraction b = new BigFraction(1000.25);
-            BigFraction c = new BigFraction(2000.5);
-            BigFraction d = a + b;
-            Assert.Equal(c, d);
+            BigFraction b = new BigFraction(2000.5);
+            BigFraction c = a + 1000.25;
+            BigFraction d = a + new decimal(1000.25);
+            Assert.Equal(b, c);
+            Assert.Equal(b, d);
         }
 
         [Fact(DisplayName = "Operator -")]
@@ -176,47 +173,48 @@ namespace OpearatorTest
         {
             BigFraction a = new BigFraction(2000.5);
             BigFraction b = new BigFraction(1000.25);
-            BigFraction c = new BigFraction(1000.25);
-            BigFraction d = a - b;
-            Assert.Equal(c, d);
+            BigFraction c = a - 1000.25;
+            BigFraction d = a - new decimal(1000.25);
+            Assert.Equal(b, c);
+            Assert.Equal(b, d);
         }
 
         [Fact(DisplayName = "Operator *")]
         public void Mulplicate()
         {
             BigFraction a = new BigFraction(-5.25);
-            BigFraction b = new BigFraction(10.1);
-            BigFraction c = new BigFraction(-53.025);
-            BigFraction d = a * b;
-            Assert.Equal(c, d);
+            BigFraction b= new BigFraction(-53.025);
+            BigFraction c = a * 10.1;
+            BigFraction d = a * new decimal(10.1);
+            Assert.Equal(b, c);
+            Assert.Equal(b, d);
         }
 
         [Fact(DisplayName = "Operator /")]
         public void Divide()
         {
-            BigFraction a = new BigFraction(5.25);
-            BigFraction b = new BigFraction(1.25);
-            BigFraction c = new BigFraction(4.20);
-            BigFraction d = a / b;
-            Assert.Equal(c, d);
+            BigFraction a = new BigFraction(-5.25);
+            BigFraction b = new BigFraction(4.20);
+            BigFraction c = a / -1.25;
+            BigFraction d = a / new decimal(-1.25);
+            Assert.Equal(b, c);
+            Assert.Equal(b, d);
         }
 
         [Fact(DisplayName = "Operator %")]
         public void Module()
         {
             BigFraction a = new BigFraction(5.25);
-            BigInteger b = new BigInteger(5);
-            BigFraction c = new BigFraction(0.25);
-            BigFraction d = a % b;
-            Assert.Equal(c, d);
+            BigFraction b = new BigFraction(0.25);
+            BigFraction c = a % 5;
+            Assert.Equal(b, c);
         }
 
         [Fact(DisplayName = "Operator >")]
         public void Greater()
         {
             BigFraction a = new BigFraction(5.25);
-            BigFraction b = new BigFraction(4.20);
-            bool isGreater = a > b;
+            bool isGreater = a > 4.2;
             Assert.True(isGreater);
         }
 
@@ -224,8 +222,7 @@ namespace OpearatorTest
         public void Samller()
         {
             BigFraction a = new BigFraction(-251.15);
-            BigFraction b = new BigFraction(4.20);
-            bool isSmaller = a < b;
+            bool isSmaller = a < 4.2;
             Assert.True(isSmaller);
         }
 
@@ -233,8 +230,9 @@ namespace OpearatorTest
         public void GreaterEqual()
         {
             BigFraction a = new BigFraction(251.15);
-            BigFraction b = new BigFraction(4.20);
-            bool isGreater = a >= b;
+            bool isGreater = a >= 251.15;
+            Assert.True(isGreater);
+            isGreater = a >= 4.2;
             Assert.True(isGreater);
         }
 
@@ -242,8 +240,9 @@ namespace OpearatorTest
         public void SamllerEqual()
         {
             BigFraction a = new BigFraction(-251.15);
-            BigFraction b = new BigFraction(4.20);
-            bool isSmaller = a <= b;
+            bool isSmaller = a <= 4.2;
+            Assert.True(isSmaller);
+            isSmaller = a <= -251.15;
             Assert.True(isSmaller);
         }
 
@@ -251,9 +250,34 @@ namespace OpearatorTest
         public void Equal()
         {
             BigFraction a = new BigFraction(-251.15);
-            BigFraction b = new BigFraction(-251.15);
-            bool isSmaller = a == b;
+            bool isSmaller = a == -251.15;
             Assert.True(isSmaller);
+        }
+    }
+}
+
+namespace ToStringTest
+{
+    public class ToStringTest
+    {
+        private readonly ITestOutputHelper output;
+
+        public ToStringTest(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
+
+        [Fact(DisplayName = "Test BigFraction Output")]
+        public void IntConstructor()
+        {
+            BigFraction a = new BigFraction(-10);
+            output.WriteLine(a.ToString());
+            a = new BigFraction(21321.12);
+            output.WriteLine(a.ToString());
+            a = new BigFraction(new decimal(451.50));
+            output.WriteLine(a.ToString());
+            a = new BigFraction(new BigInteger(10), new BigInteger(20));
+            output.WriteLine(a.ToString());
         }
     }
 }
