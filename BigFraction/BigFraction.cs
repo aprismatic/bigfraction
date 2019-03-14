ca9a36c94120ed5b50e27489db3972846e9d46e7
Calvin Che <calvin@cczy.io>
2019-03-14 20:23:16 +0800

    Update .Equals()

--- BigFraction/BigFraction.cs
+++ BigFraction/BigFraction.cs
@@ -191,12 +191,12 @@ namespace Aprismatic
         //Override Equals
         public override bool Equals(object obj)
         {
-            if (obj == null) { return false; }
-
-            BigFraction comparebigfrac = (BigFraction)obj;
-            if (Numerator == 0 && comparebigfrac.Numerator == 0) { return true; }    //If both values are zero
-
-            return Numerator * comparebigfrac.Denominator == comparebigfrac.Numerator * Denominator;
+            if (obj is BigFraction comparebigfrac)
+            {
+                if (Numerator == 0 && comparebigfrac.Numerator == 0) { return true; }    //If both values are zero
+                return Numerator * comparebigfrac.Denominator == comparebigfrac.Numerator * Denominator;
+            }
+            return false;
         }
 
         //Override GetHashCode
